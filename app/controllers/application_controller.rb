93aa2e00d7f454fa59463f49f494986020342371
RYOSUKE-PC\ryosuke <ryoryo-rk@outlook.jp>
2017-09-07 19:04:28 +0900

    sign-up　
    サインアップ機能の実装。
    アップデート時のキーを追記。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up, key: [:name])
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	devise_parameter_sanitizer.permit(:account_updates, keys: [:name])
   end
 end
