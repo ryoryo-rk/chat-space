04aeaba0dede9894763860c5799afb664e94df84
RYOSUKE-PC\ryosuke <ryoryo-rk@outlook.jp>
2017-09-13 15:43:37 +0900

    ChatSpace　グループ機能実装
    ・改行のプラス
    ・グループコントローラーでのインデントの修正
    ・group.scssのカラー変数の定義
    ・ハッシュロケットの記述改善

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,13 +16,12 @@ class GroupsController < ApplicationController
   def edit
   end
 
-
   def update
   end
 
   private
   def group_params
-  	params.require(:group).permit(:name, {user_ids: []})
+    params.require(:group).permit(:name, {user_ids: []})
   end
 
 end
