ca5eddd1b8844407af9c01acd3867390e1c59a36
RYOSUKE-PC\ryosuke <ryoryo-rk@outlook.jp>
2017-09-13 14:42:06 +0900

    ChatSpace　グループ作成機能の実装
    ・グループ作成先へのアクセスの設定
    ・編集先へのアクセスの設定
    ・グループコントローラーの改善

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,28 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+  end
+
+  def create
+  	@group = Group.new(group_params)
+    if @group.save
+      redirect_to :root, notice: "グループの作成に成功しました!"
+    else
+      redirect_to new_group_path, alert: "入力は正しいですか？"
+    end
+  end
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  private
+  def group_params
+  	params.require(:group).permit(:name, {user_ids: []})
   end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
+  has_many :users, through: :users_group
+  has_many :messages
+  has_many :users_groups
 end
