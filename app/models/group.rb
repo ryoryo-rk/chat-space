3e308df6e41be74ca577f7fa7dcb1c4f725b62e5
RYOSUKE-PC\ryosuke <ryoryo-rk@outlook.jp>
2017-09-15 21:33:09 +0900

    ChatSpace　メッセージ送信機能の実装
    ・メッセージコントローラーの記述
    ・ルーティングの追加
    ・ビューの編集
    ・フラッシュメッセージの記述
    ・バリデーションの記述

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,15 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users_groups
   validates :name, presence: true
+  def message_display
+    if messages.present?
+      if messages.last.content.present?
+        messages.last.content
+      else
+        "画像が投稿されています。"
+      end
+    else
+      "メッセージはありません。"
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
+  mount_uploader :image, ImageUploader
+  validates :content_or_image, presence: true
+  private
+  def content_or_image
+    content.presence or image.presence
+  end
 end
